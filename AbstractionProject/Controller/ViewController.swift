a8bceec2c08958c54178a3f0df7fb42436045b8b
Zac <zombiezac@att.net>
2018-12-04 13:18:26 -0700

    Initial commit

---
+++ AbstractionProject/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AbstractionProject
+//
+//  Created by Durham, Zachary on 12/4/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class ViewController: UIViewController
+{
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
